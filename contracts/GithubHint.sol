0a4a9708306f613da4d4b8c65f0575488545ebcf
André Silva <andre.beat@gmail.com>
2018-06-01 13:45:22 +0100

    update readme

--- contracts/GithubHint.sol
+++ contracts/GithubHint.sol
@@ -14,7 +14,7 @@
 //! See the License for the specific language governing permissions and
 //! limitations under the License.
 
-pragma solidity ^0.4.6;
+pragma solidity ^0.4.22;
 
 
 contract GithubHint {
